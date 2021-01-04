02e5c66d70a3ac0a902f29cdd549f7ccc9772155
cleeag <jack0506000@gmail.com>
2021-01-03 22:18:50 -0800

    basic authorization finished

--- routes/users.js
+++ routes/users.js
@@ -32,6 +32,7 @@ router.get('/login', (req, res) => {
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
   req.flash('success', 'welcome back!');
   const redirectUrl = req.session.returnTo || '/campgrounds';
+  console.log(redirectUrl)
   delete req.session.returnTo;
   res.redirect(redirectUrl);
 })
